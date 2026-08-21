c40fc0b8511473ae22c1e6df1bd848243ff7bb79
sivadineshkammara <sivadineshkammara@gmail.com>
2026-08-21 21:25:11 +0200

    ADDED: core type and common definitions

---
+++ include/common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define STEGO_MAGIC "STG1"
+#define STEGO_MAGIC_SIZE 4
+
+#endif
---
+++ include/types.h
@@ -0,0 +1,23 @@
+/*
+ *  Name:      types.h
+ *  Does:      To create a global types for the projects to maintain consistent language.
+ *  Contains:  Enumerated values.
+*/
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum
+{
+    OP_ENCODE,
+    OP_DECODE,
+    OP_INALID
+} Operations;
+
+typedef enum
+{
+    STATUS_SUCCESS,
+    STATUS_FAILURE,
+} Status;
+
+#endif
