5f68869d45bc4afb5e3d867c2c37e096caf42547
sivadineshkammara <sivadineshkammara@gmail.com>
2026-08-23 21:09:41 +0200

    Connecting Operations to Main.c

---
+++ src/decode.c
@@ -0,0 +1,12 @@
+#include "decode.h"
+
+Status decode_image(DecodeContext *context)
+{
+    (void)context;
+    return STATUS_SUCCESS;
+}
+
+void decode_cleanup(DecodeContext *context)
+{
+    (void)context;
+}
---
+++ src/encode.c
@@ -0,0 +1,12 @@
+#include "encode.h"
+
+Status encode_image(EncodeContext *context)
+{
+    (void)context;
+    return STATUS_SUCCESS;
+}
+
+void encode_cleanup(EncodeContext *context)
+{
+    (void)context;
+}
